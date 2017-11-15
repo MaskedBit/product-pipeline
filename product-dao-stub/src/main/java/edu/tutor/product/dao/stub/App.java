43cb487fa985aaff40f3ed9428337c4e3f028059
Steve Boylan <sboylan@smartplane.aero>
2017-11-15 16:44:41 -0500

    Product service template

---
+++ product-dao-stub/src/main/java/edu/tutor/product/dao/stub/App.java
@@ -0,0 +1,13 @@
+package edu.tutor.product.dao.stub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
